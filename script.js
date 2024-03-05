215dda54f544008566775fbc70f09f2c9ea37885
CastorDisaster <alexmnevins@gmail.com>
2024-03-05 10:57:20 -0500

    restructure for object-oriented approach

--- script.js
+++ script.js
@@ -1,26 +1,46 @@
-console.log('script ready!')
-
 //ALL validation will be performed in Javascript
 //all fields will be given custom error messages and will be validated when unfocused.
 
-const submit = document.getElementById('submit');
-const email = document.getElementById('email');
-const country = document.getElementById('country');
-const zip = document.getElementById('zip');
-const pass = document.getElementById('pass');
-const passConfirm = document.getElementById('passConfirm');
+const form = document.getElementById('main')
 
 
-//email must be of correct type
-email.addEventListener('input', () => {
-    if (email.validity.typeMismatch) {
-        console.log('detected');
-        showError('email');
-    }
-});
-
-
-//function for appending an error message when a type mismatch occurs
-function showError (ref) {
-    
+const formData = { //object that tracks the data collected
+    email: form.elements['email'].value,
 }
+
+const validationRules = { //object tracking which rules apply to which items
+    email: {
+        required: true,
+        minLength: 3,
+        maxLength: 20,
+    }
+}
+
+const formValidator = { //validation logic
+    validateField (fieldName, value) {
+        console.log('validating ' + fieldName)
+    },
+    validateForm (formData) {
+
+    },
+}
+
+//add event listeners to all fields
+const formFields = document.querySelectorAll('input[name]')
+formFields.forEach(field => {
+    field.addEventListener('input', function () {
+        const fieldName = this.getAttribute('name')
+        const value = this.value
+        console.log(fieldName + ": " + value)
+
+        const isValid = formValidator.validateField (fieldName, value) // how validation is called
+
+        //handling validation result
+        const errorText = document.querySelector(`#${fieldName} + span.error`)
+        if (!isValid) {
+            errorText.textContent = 'hello!'
+        }
+
+
+    })
+})
